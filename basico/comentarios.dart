115915aa5a8fa9aac47e11bba1226ea4b04b2852
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-05 15:18:00 +0200

    Creado archivo comentarios.dart con su logica

---
+++ basico/comentarios.dart
@@ -0,0 +1,23 @@
+main() {
+// Esta es una variable temporal que quiero definir
+  int a = 10;
+
+
+  final personas = [
+    //Esta es la primera persona
+    'Fernando',
+//Esta es la segunda persona
+    'Pedro',
+//Esta es la tercera persona
+    'Carlos'
+  ];
+
+
+/* comentario de multilinea*/ 
+
+  /// comentario para documentacion
+  
+  /** comentario para documentacion*/
+}
+
+
