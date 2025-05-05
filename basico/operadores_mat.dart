9df88b354a973c8efd30c069451074a1bb7633c0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-05 15:19:50 +0200

    Creado archivo operadores_mat.dart con su logica

---
+++ basico/operadores_mat.dart
@@ -0,0 +1,30 @@
+/**
+ * un operador es un simbolo que le dice al compilador
+ * que debe de realizar una tarea
+ * matematica, relacional o logica
+ * y debe de producir un resultado
+ */
+
+
+main() {
+  int a = 10 + 5; // + 15
+  a = 20 - 10; // - 10
+  a = 10 * 2; // * 20
+
+
+  double b = 10 / 2; //  / 5
+  b = 10.0 % 3; // 1
+  b = -b; // -b  es usado para cambiar el valor de la b
+
+
+  int c = 10 ~/ 3; // ~/ 3 division comun y parte entera
+
+
+  int d = 1;
+
+
+  d++;
+  d--;
+  d += 2;
+ 
+}
