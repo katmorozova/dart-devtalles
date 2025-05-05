08820fe07c6dfb65bf4cc40a47a9834c4ab237d9
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-05 15:12:16 +0200

    Creado archivo finalconst con su logica

---
+++ basico/finalconst.dart
@@ -0,0 +1,16 @@
+main() {
+  final personasFinal = ['Juan', 'Pedro', 'Fernando'];
+  const personasConst = ['Juan', 'Pedro', 'Fernando'];
+
+
+  personasFinal.add('Maria');
+  //personasConst.add('Maria'); no se puede añadir en la lista
+
+
+  print(personasFinal);
+  //print(personasConst);
+
+
+  double x = 10.25;
+print(x);
+}
