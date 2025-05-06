801a6d838afbfbdcf8444e833d02be4ac14f051f
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-06 15:01:41 +0200

    Cambios en funcion_basic.dart añadidos

--- funciones/argumentos.dart
+++ funciones/argumentos.dart
@@ -5,6 +5,19 @@ void saludar1(String mensaje,
   print('$mensaje $nombre  - $edad');
 }
 
+void saludar2({String? mensaje, required String nombre, int veces = 10}) {
+  //cuerpo de la funcion
+  print('Saludar2: $mensaje $nombre - $veces');
+}
+
+void saludar3(String mensaje, { required String nombre, int edad = 23}) { //argumento posicional
+  //cuerpo de la funcion
+  print('Saludar3: $mensaje $nombre - $edad');
+}
+
 void main(List<String> args) {
+
   saludar("Hola mundo!");
+  saludar2(veces: 23, nombre: 'Tony');
+  saludar3('Saludos', edad: 12, nombre: 'Tony');
 }
