a01e54e5000e2032aacbb08e5032a12f81c05a78
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-06 11:19:58 +0200

    Cambios añadidos en archivo funcion_basic.dart

--- funciones/funcion_basic.dart
+++ funciones/funcion_basic.dart
@@ -1,9 +1,22 @@
+/*
 main() {
   var mensaje = saludar();
   print(mensaje);
+  
 }
 
 void saludar() {
   print('Hola Fernando');
   print('Hola Maria');
+}
+*/
+
+int main() {
+  var mensaje = saludar();
+  print(mensaje);
+  return 100;
+}
+
+String saludar() {
+  return 'Hola Fernando';
 }
