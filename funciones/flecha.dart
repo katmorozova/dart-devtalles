2a7a670034227fda8d191758ce8ff32d7ee72b41
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-06 16:15:50 +0200

    Creado archivo flecha.dart con su logica

---
+++ funciones/flecha.dart
@@ -0,0 +1,22 @@
+main() {
+  int a = 10, b = 20;
+  int resultado = sumarFlecha(10, 20);
+
+  List<int> listado = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10, 10, 1];
+  var newList = listado.where((numero) {
+    return numero > 4;
+  });
+
+  var nuevoListado = listado.where((n) => n > 4);
+
+  print(resultado);
+  print(newList);
+  print(nuevoListado.toSet());
+  print(nuevoListado.toSet().toList());
+}
+
+int sumar(int x, int y) {
+  return x + y;
+}
+
+int sumarFlecha(int x, int y) => x + y;
