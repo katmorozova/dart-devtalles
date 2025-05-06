e79f80a104f31795167f48a3ce58078a235f4d55
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-06 09:04:38 +0200

    Creado archivo dowhile.dart con su logica

---
+++ control/dowhile.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+main() {
+  String continuar = 'y';
+  int contador = 0;
+
+  do {
+    //una condicion que tiene que regresar un valor booleano
+    //si condicion se cumpla - se ejecuta el codigo aqui, sino salta a la siguiente accion
+    contador++;
+    stdout.writeln('Contador: $contador');
+
+    stdout.writeln('desea continuar? (y/n)');
+
+    continuar = stdin.readLineSync() ?? 'n';
+  } while (continuar == 'y');
+}
