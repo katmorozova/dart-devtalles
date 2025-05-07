46999fd9b7972165e882623574f9dc2d880f2f26
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-07 09:18:23 +0200

    Cambios en queue.dart añadidos

---
+++ tiposNoComunes/queue.dart
@@ -0,0 +1,14 @@
+import 'dart:collection';
+
+main() {
+  Queue<int> cola = new Queue();
+
+  cola.addAll([10, 20, 30, 40, 50]);
+  print(cola);
+
+  Itarator i = cola.iterator;
+
+  while (i.moveNext()) {
+    print(i.current);
+  }
+}
