0d7050eee5eea84a7b7c234a6a31154c2e6028d1
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-07 09:19:29 +0200

    cambios en archivo queue.dart añadidos

--- tiposNoComunes/queue.dart
+++ tiposNoComunes/queue.dart
@@ -6,9 +6,12 @@ main() {
   cola.addAll([10, 20, 30, 40, 50]);
   print(cola);
 
-  Itarator i = cola.iterator;
+  /*Itarator i = cola.iterator;
 
   while (i.moveNext()) {
     print(i.current);
   }
+
+  /// Itarator da error!!!!
+  */
 }
