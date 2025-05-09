e8d692ca8f098f8eb0c876579e3212a03de0bff3
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-09 08:48:16 +0200

    Cambios en archivo streams.dart añadidos

--- tiposNoComunes/streams.dart
+++ tiposNoComunes/streams.dart
@@ -1,1 +1,23 @@
-main() {}
+import 'dart:async';
+
+main() {
+  final streamController = StreamController();
+
+  streamController.stream.listen((data) => print('Despegando! $data'),
+      onError: (err) => print('Error! $err'),
+      cancelOnError:
+          true //cancelar error, despues no ejecuta programa y no recibimos informacion
+      );
+
+  streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, tenemos un problema');
+  streamController.sink.add('Apollo 14');
+
+  streamController.sink.close();
+
+  
+
+  print('Fin del main');
+}
