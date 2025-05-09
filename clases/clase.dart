e07e246c0cbb7c8ae9685ee29eff7c3d8cf77ed0
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-09 09:34:29 +0200

    Creado archivo persona.dart y añadidos los cambios en archivo clase.dart

--- clases/clase.dart
+++ clases/clase.dart
@@ -1,29 +1,12 @@
+import 'persona.dart';
+
 main() {
   final persona = new Persona();
-  persona.nombre = "Fernando";
-  persona.age = 34;
-  persona.bio = "Nacio por ahi";
+  persona..nombre = "Fernando"
+  //persona.age = 34;
+  //persona.bio = "Nacio por ahi";
+        ..age = 34
+        ..bio = "Nacio por ahi";
 
-  print(persona.toString());
-}
-
-class Persona {
-//Campos o propiedad
-  String? nombre;
-  int? age;
-  String? bio;
-
-//Get y sets
-
-//Constructores
-
-//Metodos(funsiones)
-@override
-  String toString() {
-    // TODO: implement toString
-    return '$nombre $age $bio';
-  }
-
-
-
+  print(persona);
 }
---
+++ clases/persona.dart
@@ -0,0 +1,17 @@
+class Persona {
+//Campos o propiedad
+  String? nombre;
+  int? age;
+  String? bio;
+
+//Get y sets
+
+//Constructores
+
+//Metodos(funsiones)
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$nombre $age $bio';
+  }
+}
