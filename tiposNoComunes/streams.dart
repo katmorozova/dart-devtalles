6ae1b42fde73a3a974abfd876e4924023ec9223a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-09 09:01:12 +0200

    Añadidos cambios en archivo streams.dart

--- tiposNoComunes/streams.dart
+++ tiposNoComunes/streams.dart
@@ -1,14 +1,25 @@
 import 'dart:async';
 
 main() {
-  final streamController = StreamController();
+  //final streamController = StreamController();
+  //final streamController = new StreamController<String>();
+  final streamController = new StreamController<String>.broadcast();//transforma stream en multiples suscripciones
+  
 
   streamController.stream.listen((data) => print('Despegando! $data'),
       onError: (err) => print('Error! $err'),
+      onDone:() => print('Mission completa!'),
       cancelOnError:
           true //cancelar error, despues no ejecuta programa y no recibimos informacion
       );
+  streamController.stream.listen((data) => print('Despegando Stream2! $data'),
+      onError: (err) => print('Error Stream 2! $err'),
+      onDone:() => print('Mission completa Stream 2!'),
+      cancelOnError:
+          true //cancelar error, despues no ejecuta programa y no recibimos informacion
+      );    
 
+  streamController.sink.add('Apollo 10');
   streamController.sink.add('Apollo 11');
   streamController.sink.add('Apollo 12');
   streamController.sink.add('Apollo 13');
