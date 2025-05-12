8ae300396bd5612150ced8a0b2261777a169ea0a
Ekaterina Morozova <90331828+katmorozova@users.noreply.github.com>
2025-05-12 11:08:55 +0200

    Cambios en archivos clase.dart y persona.dart añadidos

--- clases/clase.dart
+++ clases/clase.dart
@@ -2,11 +2,14 @@ import 'persona.dart';
 
 main() {
   final persona = new Persona();
-  persona..nombre = "Fernando"
-  //persona.age = 34;
-  //persona.bio = "Nacio por ahi";
-        ..age = 34
-        ..bio = "Nacio por ahi";
+  persona
+    ..nombre = "Fernando"
+    //persona.age = 34;
+    //persona.bio = "Nacio por ahi";
+    ..age = 34;
+  //..bio = "Nacio por ahi";
 
-  print(persona);
+  persona.bio = 'Cambie el valor!';
+
+  print(persona.bio);
 }
--- clases/persona.dart
+++ clases/persona.dart
@@ -2,9 +2,20 @@ class Persona {
 //Campos o propiedad
   String? nombre;
   int? age;
-  String? bio;
+  //String? bio;
+  String _bio = 'Hola, soy una propiedad privada.';
 
 //Get y sets
+
+  String get info {
+    return 'Hola mundo desde Persona!';
+  }
+
+  String get bio => _bio.toUpperCase();
+
+  set bio(String texto) {
+    _bio = texto;
+  }
 
 //Constructores
 
